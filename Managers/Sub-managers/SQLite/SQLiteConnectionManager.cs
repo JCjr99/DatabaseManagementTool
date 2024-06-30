6c6b753d09bedb30cb7dd3f5a1790ba23a9ed1c4
Jacob Cooper <jacob.cooper448@gmail.com>
2024-06-30 01:34:18 +0100

    Updated Connection Manager
    Added generic interface and made current implementation into the SQLite specific implementation

---
+++ Managers/Sub-managers/SQLite/SQLiteConnectionManager.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace DatabaseManagementTool
+{
+    public class SQLiteConnectionManager : IDBConnectionManager
+    {
+        private readonly string connectionString;
+
+        public SQLiteConnectionManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public SQLiteConnection CreateConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+
+        // Explicitly implement the IDBConnectionManager interface's method
+        T IDBConnectionManager.CreateConnection<T>()
+        {
+            if (typeof(T) == typeof(SQLiteConnection))
+            {
+                return (T)(object)new SQLiteConnection(connectionString);
+            }
+
+            throw new InvalidOperationException("Invalid connection type requested.");
+        }
+    }
+
+}
