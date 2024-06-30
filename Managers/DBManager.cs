0c783cbf5d4dc62ab90f22b528dcac4d12aca52a
Jacob Cooper <jacob.cooper448@gmail.com>
2024-06-30 01:49:47 +0100

    Updated DBManager to be Completely Generic
    DBManager is now completely generic. It will take in a connection string and a DBType and use that to create the relevant managers for that type using the factory thus allowing any set of managers to be used with this class and makes its use even easier.

--- Managers/DBManager.cs
+++ Managers/DBManager.cs
@@ -1,50 +1,48 @@
-﻿using DatabaseManagementTool.Managers.Sub_managers;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DatabaseManagementTool.Managers
+namespace DatabaseManagementTool
 {
-    public class DBManager
+    public class DBManager: IDBCreationManager, IDBReadingManager, IDBDeletionManager
     {
-        private readonly DBConnectionManager connectionManager;
-        private readonly DBTableManager tableManager;
-        private readonly DBDataInserter dataInserter;
-        private readonly DBDataRetriever dataRetriever;
-
-        public DBManager(string connectionString)
+        private readonly IDBConnectionManager connectionManager ;
+        private readonly IDBCreationManager creationManager;
+        private readonly IDBReadingManager readingManager;
+        private readonly IDBDeletionManager deletionManager;
+        public DBManager(string connectionString, DBType dbtype)
         {
-            connectionManager = new DBConnectionManager(connectionString);
-            tableManager = new DBTableManager(connectionManager);
-            dataInserter = new DBDataInserter(connectionManager);
-            dataRetriever = new DBDataRetriever(connectionManager);
-        }
-
-        public async Task<Guid> InsertObjectAsync(object obj, CancellationToken cancellationToken = default)
-        {
-            return await dataInserter.InsertObjectAsync(obj, cancellationToken);
-        }
-
-        public async Task ReadAllAsync(Type table, CancellationToken cancellationToken = default)
-        {
-            await dataRetriever.ReadAllAsync(table, cancellationToken);
-        }
-
-        public async Task GetTableInfoAsync(Type table, CancellationToken cancellationToken = default)
-        {
-            await dataRetriever.GetTableInfoAsync(table, cancellationToken);
+            (connectionManager, creationManager, readingManager, deletionManager) = DBManagerFactory.CreateDBManager(dbtype, connectionString);
         }
 
         public async Task CreateTableAsync(Type type, CancellationToken cancellationToken = default)
         {
-            await tableManager.CreateTableAsync(type, cancellationToken);
+            await creationManager.CreateTableAsync(type, cancellationToken);
         }
 
+        public async Task<Guid> InsertObjectAsync(object obj, CancellationToken cancellationToken = default)
+        {
+            return await creationManager.InsertObjectAsync(obj, cancellationToken);
+        }
+
+        public async Task ReadAllAsync(Type table, CancellationToken cancellationToken = default)
+        {
+            await readingManager.ReadAllAsync(table, cancellationToken);
+        }
+
+        public async Task GetTableInfoAsync(Type table, CancellationToken cancellationToken = default)
+        {
+            await readingManager.GetTableInfoAsync(table, cancellationToken);
+        }
+
+    
         public async Task DeleteTableAsync(Type type, CancellationToken cancellationToken = default)
         {
-            await tableManager.DeleteTableAsync(type, cancellationToken);
+            await deletionManager.DeleteTableAsync(type, cancellationToken);
         }
     }
 }
