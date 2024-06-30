ae6f11a91a70f5020f713b40dd3645effd1b8d08
Jacob Cooper <jacob.cooper448@gmail.com>
2024-06-30 01:42:54 +0100

    Updated TableManager to DeletionManager
    The previous TableManager class has been converted to a DeletionManager with an interface to match. This is part of a redesign of the sub-managers so that there is a sub manager class for each of the CRUD operations. This class will deal with all operations surrounding deletion in the database.

---
+++ Managers/Sub-managers/SQLite/SQLiteDeletionManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManagementTool
+{
+    public class SQLiteDeletionManager: IDBDeletionManager
+    {
+        private readonly SQLiteConnectionManager connectionManager;
+
+        public SQLiteDeletionManager(SQLiteConnectionManager connectionManager)
+        {
+            this.connectionManager = connectionManager;
+        }
+
+       
+
+        public async Task DeleteTableAsync(Type type, CancellationToken cancellationToken = default)
+        {
+            using (var connection = connectionManager.CreateConnection())
+            {
+                var command = connection.CreateCommand();
+                string tableName = type.Name;
+
+                string query = $"DROP TABLE IF EXISTS {tableName}";
+
+                command.CommandText = query;
+                await command.ExecuteNonQueryAsync(cancellationToken);
+            }
+            Console.WriteLine($"Table \"{type.Name}\" deleted successfully");
+        }
+
+      
+    }
+}
