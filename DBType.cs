f52f8abf667bcde91447077c4e128a6b0166c1ff
Jacob Cooper <jacob.cooper448@gmail.com>
2024-06-30 01:47:43 +0100

    Added Factory to Create Different DBManagers
    The factory class will use the new DBType to determine what type of DB Manager is to be created then will create a ConnectionManager, CreationManager, DeletionManager and ReadingManager objects of the corresponding type given all in one simple factory. UpdateManager will be added to this once it is created.

---
+++ DBType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManagementTool
+{
+    public enum DBType
+    {
+        SQLite
+        // Add other database types here as needed
+    }
+}
---
+++ Factories/DBManagerFactory.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManagementTool
+{
+    public static class DBManagerFactory
+    {
+        
+
+        public static (IDBConnectionManager, IDBCreationManager, IDBReadingManager, IDBDeletionManager ) CreateDBManager(DBType dbType, string connectionString)
+        {
+            switch (dbType)
+            {
+                case DBType.SQLite:
+                    var connectionManager = new SQLiteConnectionManager(connectionString);
+                    var creationManager = new SQLiteCreationManager(connectionManager);
+                    var readingManager = new SQLiteReadingManager(connectionManager);
+                    var deletionManager = new SQLiteDeletionManager(connectionManager);
+                    
+                    return (connectionManager, creationManager, readingManager, deletionManager);
+                // Add other cases here for different DB types
+                default:
+                    throw new ArgumentException("Invalid database type", nameof(dbType));
+            }
+        }
+    }
+
+}
